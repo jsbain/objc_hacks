67f71d75c708d70fd8400f465ce1125faa3ba8ac
jsbain <jsbain>
2015-07-25 08:04:36 +0000

    initial commit

---
+++ objcstuff.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+import objc_util,ui
+
+v=ui.View()
+
+vv=objc_util.ObjCInstance(v._objc_ptr)
+
+wnd= objc_util.ObjCClass('UIWindow').alloc()
+r=objc_util.CGRect(objc_util.CGPoint(0,0),objc_util.CGSize(100,100))
+
+wnd.initWithFrame_(r)
